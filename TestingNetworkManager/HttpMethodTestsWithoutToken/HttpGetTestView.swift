eae07dcc225c972170622ec330262c4551e4f8ca
Muhammed Mahmood <muhammedmahmood999@gmail.com>
2022-11-06 19:09:20 +0000

    Using my own API to test http GET requests.

--- TestingNetworkManager/HttpMethodTestsWithoutToken/HttpGetTestView.swift
+++ TestingNetworkManager/HttpMethodTestsWithoutToken/HttpGetTestView.swift
@@ -11,42 +11,64 @@ import SwiftUI
  THIS SEEMS TO BE WORKING FINE!!!!
  
  */
-// For: https://dummyjson.com/products/1
-struct Welcome: Codable {
+// For: http://127.0.0.1:8000/posts/1
+struct Post: Codable {
+    let title, content: String
+    let published: Bool
     let id: Int
-    let title, welcomeDescription: String
-    let price: Int
-    let discountPercentage, rating: Double
-    let stock: Int
-    let brand, category: String
-    let thumbnail: String
-    let images: [String]
+    let createdAt: String
+    let ownerID: Int
+    let owner: Owner
 
     enum CodingKeys: String, CodingKey {
-        case id, title
-        case welcomeDescription = "description"
-        case price, discountPercentage, rating, stock, brand, category, thumbnail, images
+        case title, content, published, id
+        case createdAt = "created_at"
+        case ownerID = "owner_id"
+        case owner
+    }
+}
+
+struct Owner: Codable {
+    let id: Int
+    let email, createdAt: String
+
+    enum CodingKeys: String, CodingKey {
+        case id, email
+        case createdAt = "created_at"
     }
 }
 
 struct HttpGetTestView: View {
+    @State private var title: String = ""
+    @State private var content: String = ""
+    @State private var id: Int = 1
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("User with the id \(id) just published")
+                .font(.title)
+                .padding()
+            VStack{
+                Text("Title: \(title)")
+                Text("\(content)")
+            }
+            
+            Spacer()
+            
         }
         .padding()
         .task {
             do {
-                var data: Welcome
-                
-                (data, _ ) = try await NetworkManager.shared.get(with: "https://dummyjson.com/products/1")
-                let _ = print("data.id:", data.id)
+                let data: Post = try await NetworkManager.shared.get(
+                    with: PostApi.getSinglePost(userID: 1).path
+                )
+                title = data.title
+                content = data.content
+                id = data.id
                 let _ = print("data.title:", data.title)
-                let _ = print("data.price:", data.price)
-                let _ = print("data.discountPercentage:", data.discountPercentage)
+                let _ = print("data.content:", data.content)
+                let _ = print("data.id:", data.id)
+                let _ = print("data.createdAt:", data.createdAt)
                 let _ = print("Complete Data Object:", data)
                
             } catch {
