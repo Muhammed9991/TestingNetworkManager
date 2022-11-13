948cf0d89f8a55fd7d36212920f8ce1d387ea2b9
Muhammed Mahmood <muhammedmahmood999@gmail.com>
2022-11-13 19:07:56 +0000

    Add: error codes for when adding data to keychain

--- TestingNetworkManager/NetworkingModule/AuthManager.swift
+++ TestingNetworkManager/NetworkingModule/AuthManager.swift
@@ -7,10 +7,19 @@
 
 import Foundation
 
-struct Token: Decodable {
-    let isValid: Bool
-    let value: String
+enum KeychainError: Error {
+    // Attempted read for an item that does not exist.
+    case itemNotFound
     
+    // Attempted save to override an existing item.
+    // Use update instead of save to update existing items
+    case duplicateItem
+    
+    // A read of an item in any format other than Data
+    case invalidItemFormat
+    
+    // Any operation result status than errSecSuccess
+    case unexpectedStatus(OSStatus)
 }
 
 actor AuthManager {
